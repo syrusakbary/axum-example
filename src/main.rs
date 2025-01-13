7e24f49c91026b5c190fdaa935b13d50d1d9e0db
Syrus Akbary <me@syrusakbary.com>
2025-01-13 11:38:47 +0100

    Fixed to use latest axum

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ async fn main() {
     // Building our application with a single Route
     let app = Router::new().route("/", get(handler));
 
-    let port = std::env::var("PORT").unwrap_or("80".to_string());
+    let port = std::env::var("PORT").unwrap_or("3000".to_string());
     let port = port.parse::<u16>().unwrap_or_else(|_| {
         eprintln!("Invalid port number: {}", port);
         std::process::exit(1);
@@ -14,7 +14,7 @@ async fn main() {
     // Run the server with hyper on http://127.0.0.1:3000
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
     eprintln!("Listening on http://{}", addr);
-    axum::Server::bind(&addr)
+    axum_server::bind(addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
